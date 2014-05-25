b6040f51141fda04953a0075f29e4a57234464ea
Ken Ammerman <k2ammerman@gmail.com>
2014-05-25 12:14:50 -0700

    completed update

--- cachematrix.R
+++ cachematrix.R
@@ -5,10 +5,13 @@
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
+    
+    ## set function
     set <- function(y) {
       x <<- y
       m <<- NULL
     }
+    ## get function
     get <- function() x
     setmatrix <- function(matrix) m <<- matrix
     getmatrix <- function() m
@@ -18,8 +21,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve receives a matrix, if the matrix object is empty
+## the cached matrix id returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- rev(data, ...)
+    x$setmatrix(m)
+    m
 }
